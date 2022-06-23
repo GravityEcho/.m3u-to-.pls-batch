a02e95a0e3a605802461386096c8033b80fdae0e
GravityEcho <41200150+GravityEcho@users.noreply.github.com>
2022-06-23 14:14:53 -0500

    Add files via upload

---
+++ M3U to PLS.bat
@@ -0,0 +1,20 @@
+@echo off 
+setlocal enableDelayedExpansion
+
+cd /d "%~dp0"
+
+set "i=0"
+@for /f %%a in ('findstr "http" playlist.m3u') do (   
+    set /a "i+=1"
+    set "song[!i!]=%%a" 
+)   
+
+set "total=!i!"
+>playlist.pls echo [playlist]
+>playlist.pls echo NumberOfEntries=!total! 
+>playlist.pls (
+    for /l %%p in (1,1,!total!) do (
+        echo File%%p=!song[%%p]!
+    )
+)
+
